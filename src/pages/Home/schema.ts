45089991a983103941147bc6d2bb3c34538cbfa2
Mateus Seixas <mateuseixas@icloud.com>
2024-08-06 18:57:42 -0300

    fix: Validation issues

--- src/pages/Home/schema.ts
+++ src/pages/Home/schema.ts
@@ -1,14 +1,35 @@
+import { format, isAfter, isValid, parse } from "date-fns";
 import { z } from "zod";
+
+const cpfPattern = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
 
 export const requestLoanSimulationSchema = z.object({
   cpf: z
     .string({ required_error: "O seu nome é obrigatório." })
-    .min(1, "O seu nome é obrigatório."),
+    .min(1, "O seu nome é obrigatório.")
+    .regex(cpfPattern, "O formato do CPF está incorreto."),
   uf: z.enum(["MG", "SP", "ES", "RJ"], {
     required_error: "O estado é obrigatório.",
     invalid_type_error: "O estado é inválido.",
   }),
-  birthday: z.string({ required_error: "A data de nascimento é obrigatória." }),
+  birthday: z
+    .string({ required_error: "A data de nascimento é obrigatória." })
+    .refine(
+      (date) => {
+        const parsedDate = parse(date, "dd/MM/yyyy", new Date());
+        const today = new Date();
+
+        return (
+          isValid(parsedDate) &&
+          format(parsedDate, "dd/MM/yyyy") === date &&
+          !isAfter(parsedDate, today)
+        );
+      },
+      {
+        message:
+          "A data de nascimento deve ser uma data válida no formato dd/MM/yyyy e não pode ser uma data futura.",
+      }
+    ),
   loan: z
     .string({ required_error: "O valor do empréstimo é obrigatório." })
     .min(0)
